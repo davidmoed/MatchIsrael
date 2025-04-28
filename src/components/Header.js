cc93b75ceca59f3471d8b7bbae788085b89dac9c
David <davidjmoed@gmail.com>
2025-04-28 09:51:36 +0300

    Adding logo to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/Header.css";
 
+import logoUrl from "../matchIsrael_logo.png";
+
 const Header = () => {
   return (
     <header className="header">
+      <Link to="/" className="nav-logo-cont">
+        <img className="nav-logo" src={logoUrl} alt="Match Israel" />
+      </Link>
       <nav className="nav">
         <Link to="/" className="nav-link">
           Home
